8323368c320f01c1e0afe6ca528c2e3ef299dbe6
Angel Alegre <alegre.qa@gmail.com>
2023-07-21 16:18:02 -0500

    info del sistema

--- mjs/suma.mjs
+++ mjs/suma.mjs
@@ -3,9 +3,14 @@ export function suma(a, b) {
 }
 
 export function sub(a, b) {
-  return a - b;
+  if (a > b) {
+    console.log("🚀 ~ file: suma.mjs:7 ~ sub ~ a :", a);
+    return a - b;
+  }
 }
 
 export function multi(a, b) {
   return a * b;
 }
+
+//escribe una funcion radicacion en js
