d1b27eb8f9b6a1eec487bc5c3917bfdcce3bafaa
Angel Alegre <alegre.qa@gmail.com>
2023-07-24 08:43:59 -0500

    read files asincronos

--- 3.fs-readFile.js
+++ 3.fs-readFile.js
@@ -1,15 +1,16 @@
 const fs = require("node:fs");
 
 console.log("Leyendo el 1er archivo");
-// const primerText = fs.readFileSync("./archivo.txt", "utf-8");
-fs.readFile("./archivo.txt", "utf-8", (err, primerText) => {
-  console.log(primerText);
+// const primerText = fs.readFileSync("./archivo.txt", "utf-8");  //metodo sincrono
+fs.readFile("./archivo.txt", "utf-8", (err, texto) => {
+  //metodo asincrono con callbacks
+  console.log("Primer Texto: --> ", texto);
 });
 
 console.log("----------------------------->  Haciendo cositas XDDD");
 
 console.log("Leyendo el 2do archivo");
 // const segundoText = fs.readFileSync("./archivo2.txt", "utf-8");
-fs.readFile("./archivo2.txt", "utf-8", (err, segundoText) => {
-  console.log(segundoText);
+fs.readFile("./archivo2.txt", "utf-8", (err, texto) => {
+  console.log("Segundo Texto: --> ", texto);
 });
